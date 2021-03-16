80ecc97bfea1e17451ccef387f3d9e66f4d5dc48
FoxerLee <foxerlee1@gmail.com>
2021-03-15 21:42:25 -0400

    case/money bar update daily

---
+++ Assets/Scripts/StateController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateController : MonoBehaviour
+{
+    public static StateController instance = null;
+    
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+}
